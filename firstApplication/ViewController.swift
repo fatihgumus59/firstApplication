49e2da4f3cc9a89fda6335b0fabcfd0c04098dd8
Fatih Gumus <fatihgumus72@gmail.com>
2023-04-12 00:31:11 +0300

    text size and color edited

--- firstApplication/ViewController.swift
+++ firstApplication/ViewController.swift
@@ -9,12 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Fatih Gumus
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func degistirButton(_ sender: Any) {
+        label.font = label.font.withSize(28);
+        label.textColor = UIColor.red;
+        label.text = "Merhaba Dünya";
+    }
+    
+    
 }
 
